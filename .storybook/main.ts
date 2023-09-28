4e7f8b263d7b7a7f925102e8aef8b4eb9f029cf7
He <katharine.he@accenture.com>
2023-09-28 14:46:18 +1000

    updates

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,8 @@ const config: StorybookConfig = {
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-actions/register"
   ],
   framework: {
     name: "@storybook/react-webpack5",
@@ -17,5 +19,12 @@ const config: StorybookConfig = {
     autodocs: "tag",
   },
   staticDirs: ["..\\public"],
+  refs: {
+    'design-system': {
+      title: 'TPG Design System',
+      url: 'https://master--64db484a3c1156a46b68a3a8.chromatic.com',
+      expanded: false, // Optional, true by default
+    },
+  },
 };
 export default config;
